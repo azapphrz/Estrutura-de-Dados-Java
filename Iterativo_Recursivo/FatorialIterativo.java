707cecd96abec200083a7825d09e0d525701756c
Asafe Felipe <azapphrz@users.noreply.github.com>
2020-05-21 09:42:49 -0300

    Arrumando o codigo

--- Iterativo_Recursivo/FatorialIterativo.java
+++ Iterativo_Recursivo/FatorialIterativo.java
@@ -2,12 +2,7 @@ package Iterativo_Recursivo;
 
 public class FatorialIterativo {
 
-	public static void main(String[] args) {
-		int resultado = fatorialIterativo2(3);
-		  System.out.println(resultado);
-		}
-
-	public static int fatorialIterativo2(int valor) {
+	public static int fatorialIterativo1(int valor) {
 		int x = valor;
 		for (int i = 1; i<valor; i++) {
 			x= x*i;
@@ -15,11 +10,16 @@ public class FatorialIterativo {
 		return x;
 	}
 
-	public static int fatorialIterativo1(int valor) {
+	public static int fatorialIterativo2(int valor) {
 		int x = valor;
 		for (int i = valor; i>0; i--) {
 			x= x*i;
 		}
 		return x;
 	}
+	public static void main(String[] args) {
+		int resultado = fatorialIterativo2(3);
+		  System.out.println(resultado);
+		}
+
 }
